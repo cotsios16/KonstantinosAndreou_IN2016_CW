d60da546dadd2e4cc54137d70cea4d68d06770bc
Constantinos Andreou <constantinosandreou@Constantinoss-MacBook-Air.local>
2026-04-25 16:17:49 +0300

    Add invulnerability support to Spaceship

--- SRC/Spaceship.cpp
+++ SRC/Spaceship.cpp
@@ -8,88 +8,82 @@ using namespace std;
 
 // PUBLIC INSTANCE CONSTRUCTORS ///////////////////////////////////////////////
 
-/**  Default constructor. */
 Spaceship::Spaceship()
-	: GameObject("Spaceship"), mThrust(0)
+	: GameObject("Spaceship"), mThrust(0), mInvulnerableTimer(0)
 {
 }
 
-/** Construct a spaceship with given position, velocity, acceleration, angle, and rotation. */
 Spaceship::Spaceship(GLVector3f p, GLVector3f v, GLVector3f a, GLfloat h, GLfloat r)
-	: GameObject("Spaceship", p, v, a, h, r), mThrust(0)
+	: GameObject("Spaceship", p, v, a, h, r), mThrust(0), mInvulnerableTimer(0)
 {
 }
 
-/** Copy constructor. */
 Spaceship::Spaceship(const Spaceship& s)
-	: GameObject(s), mThrust(0)
+	: GameObject(s), mThrust(0), mInvulnerableTimer(0)
 {
 }
 
-/** Destructor. */
 Spaceship::~Spaceship(void)
 {
 }
 
 // PUBLIC INSTANCE METHODS ////////////////////////////////////////////////////
 
-/** Update this spaceship. */
 void Spaceship::Update(int t)
 {
-	// Call parent update function
+	// Tick down the invulnerability timer each frame
+	if (mInvulnerableTimer > 0)
+	{
+		mInvulnerableTimer -= t;
+		if (mInvulnerableTimer < 0) mInvulnerableTimer = 0;
+	}
+
+	// Call parent update
 	GameObject::Update(t);
 }
 
-/** Render this spaceship. */
 void Spaceship::Render(void)
 {
+	// When invulnerable, flash the ship by skipping render every other 200ms window
+	if (IsInvulnerable())
+	{
+		if ((mInvulnerableTimer / 200) % 2 == 0)
+			return; // skip this frame to create a blinking effect
+	}
+
 	if (mSpaceshipShape.get() != NULL) mSpaceshipShape->Render();
 
-	// If ship is thrusting
-	if ((mThrust > 0) && (mThrusterShape.get() != NULL)) {
+	if ((mThrust > 0) && (mThrusterShape.get() != NULL))
 		mThrusterShape->Render();
-	}
 
 	GameObject::Render();
 }
 
-/** Fire the rockets. */
 void Spaceship::Thrust(float t)
 {
 	mThrust = t;
-	// Increase acceleration in the direction of ship
-	mAcceleration.x = mThrust*cos(DEG2RAD*mAngle);
-	mAcceleration.y = mThrust*sin(DEG2RAD*mAngle);
+	mAcceleration.x = mThrust * cos(DEG2RAD * mAngle);
+	mAcceleration.y = mThrust * sin(DEG2RAD * mAngle);
 }
 
-/** Set the rotation. */
 void Spaceship::Rotate(float r)
 {
 	mRotation = r;
 }
 
-/** Shoot a bullet. */
 void Spaceship::Shoot(void)
 {
-	// Check the world exists
 	if (!mWorld) return;
-	// Construct a unit length vector in the direction the spaceship is headed
-	GLVector3f spaceship_heading(cos(DEG2RAD*mAngle), sin(DEG2RAD*mAngle), 0);
+	GLVector3f spaceship_heading(cos(DEG2RAD * mAngle), sin(DEG2RAD * mAngle), 0);
 	spaceship_heading.normalize();
-	// Calculate the point at the node of the spaceship from position and heading
 	GLVector3f bullet_position = mPosition + (spaceship_heading * 4);
-	// Calculate how fast the bullet should travel
 	float bullet_speed = 30;
-	// Construct a vector for the bullet's velocity
 	GLVector3f bullet_velocity = mVelocity + spaceship_heading * bullet_speed;
-	// Construct a new bullet
 	shared_ptr<GameObject> bullet
 		(new Bullet(bullet_position, bullet_velocity, mAcceleration, mAngle, 0, 2000));
 	bullet->SetBoundingShape(make_shared<BoundingSphere>(bullet->GetThisPtr(), 2.0f));
 	bullet->SetShape(mBulletShape);
-	// Add the new bullet to the game world
 	mWorld->AddObject(bullet);
-
 }
 
 bool Spaceship::CollisionTest(shared_ptr<GameObject> o)
@@ -102,5 +96,8 @@ bool Spaceship::CollisionTest(shared_ptr<GameObject> o)
 
 void Spaceship::OnCollision(const GameObjectList &objects)
 {
+	// If invulnerable, ignore asteroid collisions entirely
+	if (IsInvulnerable()) return;
+
 	mWorld->FlagForRemoval(GetThisPtr());
 }
--- SRC/Spaceship.h
+++ SRC/Spaceship.h
@@ -21,14 +21,19 @@ public:
 	virtual void Shoot(void);
 
 	void SetSpaceshipShape(shared_ptr<Shape> spaceship_shape) { mSpaceshipShape = spaceship_shape; }
-	void SetThrusterShape(shared_ptr<Shape> thruster_shape) { mThrusterShape = thruster_shape; }
-	void SetBulletShape(shared_ptr<Shape> bullet_shape) { mBulletShape = bullet_shape; }
+	void SetThrusterShape(shared_ptr<Shape> thruster_shape)   { mThrusterShape = thruster_shape; }
+	void SetBulletShape(shared_ptr<Shape> bullet_shape)       { mBulletShape = bullet_shape; }
 
 	bool CollisionTest(shared_ptr<GameObject> o);
 	void OnCollision(const GameObjectList &objects);
 
+	// Invulnerability (Part II - Invulnerability Power-up)
+	void SetInvulnerable(int duration_ms) { mInvulnerableTimer = duration_ms; }
+	bool IsInvulnerable() const           { return mInvulnerableTimer > 0; }
+
 private:
 	float mThrust;
+	int   mInvulnerableTimer; // counts down in ms; >0 means ship is invulnerable
 
 	shared_ptr<Shape> mSpaceshipShape;
 	shared_ptr<Shape> mThrusterShape;
